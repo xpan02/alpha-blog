2e1577925b8be7adafb281bc5323d3b153873fc0
Rails Udemy Course <xp336@nyu.edu>
2017-09-13 04:51:23 +0000

    add user validation for email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, 
+                       length: { minimum: 3,maximum: 25}, 
+                       uniqueness: { case_sensitive: false }
+  VALID_EMAIL_RIGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates:email, presence: true, 
+                  length: { maximum:105},
+                  uniqueness: { case_sensitive: false },
+                  format: { with: VALID_EMAIL_RIGEX,
+                  message: "valid email address only" }
 end
