73c559513c53c651259d2dbdf2a7c2757b990e34
Rails Udemy Course <xp336@nyu.edu>
2017-09-14 12:24:05 +0000

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
   def new
     @user=User.new
   end
-    
+  
+  def index
+    @users=User.all
+  end
+
   def create
     @user=User.new(user_params)
     if @user.save
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def gravatar_for(user, option = {size: 150})
+  def gravatar_for(user, option = {size: 80})
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     size = option[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
