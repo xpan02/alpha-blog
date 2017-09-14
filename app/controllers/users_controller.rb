0e366b8102d3e7da107b327089d9cd85d63de836
Rails Udemy Course <xp336@nyu.edu>
2017-09-14 13:39:40 +0000

    add all paginations to user and articles page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users=User.all
+    @users=User.paginate(:page=>params[:page], :per_page=> 5)
   end
 
   def create
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   end
   
   def show
-      
+      @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
     
   private
